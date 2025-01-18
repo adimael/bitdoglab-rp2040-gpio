0309b98b1db034cb6a327a90e424c604318adcdd
joelson-santana22 <joelson.a2744@ufob.edu.br>
2025-01-18 16:52:43 -0300

    Adição da condicional if e else no arquivo principal.

--- src/main.c
+++ src/main.c
@@ -12,6 +12,25 @@ int main(){
         printf("Digite o comando: ");
         char comando[10];
         scanf("%s", comando);
+
+        if (strcmp(comando, "vermelho") == 0) {
+            control_led(1, 0, 0);
+        } else if (strcmp(comando, "azul") == 0) {
+            control_led(0, 1, 0);
+        } else if (strcmp(comando, "verde") == 0) {
+            control_led(0, 0, 1);
+        } else if (strcmp(comando, "branco") == 0) {
+            control_led(1, 1, 1);
+        } else if (strcmp(comando, "desligar") == 0) {
+            control_led(0, 0, 0);
+        } else if (strcmp(comando, "som") == 0) {
+            control_buzzer();
+        } else if (strcmp(comando, "REBOOT") == 0) {
+            printf("Rebooting...\n");
+            reset_usb_boot(0, 0);
+        } else {
+            printf("Comando não reconhecido.\n");
+        }
     }
     
 
